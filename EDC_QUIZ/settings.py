3e2d019588a9f168f6777396f03f9ea948eb3027
N15H4N7 <ngupta_be19@thapar.edu>
2020-09-29 23:08:23 +0530

    Update settings.py

--- EDC_QUIZ/settings.py
+++ EDC_QUIZ/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
+    'questions',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
