6f7520f2588f64179013207d354c22e97422240b
N15H4N7 <ngupta_be19@thapar.edu>
2020-09-29 08:56:50 +0530

    initial

---
+++ questions/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from .forms import AnswerForm
+from .models import Questions, Answer
+
+def question(request):
+    ques = Questions.objects.filter(slot=1).order_by("ques_no")
+
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            f = form.save()
+            f.candidate = 'hellohelllo'
+            f.save()
+
+    context = { 
+        'form':AnswerForm(),
+        'questions':ques,
+    }
+    
+    print(Answer.objects.all())
+    return render(request, 'questions/index.html', context)
