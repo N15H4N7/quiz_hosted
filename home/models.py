1100fccee205e8628390ee910c319be51ce15566
Kajal <kajal11122000@gmail.com>
2020-10-22 22:01:06 +0530

    user object automation creaed

--- home/models.py
+++ home/models.py
@@ -1,9 +1,25 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.contrib.auth.base_user import BaseUserManager
 # Create your models here.
 
 class CustomUserManager(BaseUserManager):
+    def create_user(self, name, email, password=None):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
     def create_superuser(self, email, password=None):
         user = self.model(email=email, is_staff=True, is_superuser=True)
         user.set_password(password)
@@ -18,6 +34,7 @@ class User(AbstractUser):
     start_time  = models.DateTimeField(null=True, blank=True)
     points      = models.IntegerField(default=0)
 
+    objects =  UserManager()
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
--- home/views.py
+++ home/views.py
@@ -23,7 +23,7 @@ def generate_user(request):
         
         for u in data['RECORDS']:
             if not User.objects.filter(email=u['email']):
-                user =  User(name=u['name'], email=u['email'], password="edcrecruitments")
+                user =  User.bjects.create_user(name=u['name'], email=u['email'], password="edcrecruitments")
                 user.save()
     return redirect('thank-you')
 
