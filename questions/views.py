7198cce51503842c19ae323d59b185f63e1547e3
N15H4N7 <ngupta_be19@thapar.edu>
2020-10-23 19:30:44 +0530

    ...

--- questions/views.py
+++ questions/views.py
@@ -23,7 +23,7 @@ def question(request):
         u.start_time = datetime.datetime.now()
         u.save()
     
-    ques = Questions.objects.filter(slot=2).order_by("ques_no")
+    ques = Questions.objects.filter(slot=3).order_by("ques_no")
 
     if request.method == 'POST':
         form = AnswerForm(request.POST)
